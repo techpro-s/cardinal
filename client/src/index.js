f5efc5da55d96378c5a33c0d6c2871a783fd9148
gutilml <agustin.luna.m.l@gmail.com>
2017-07-16 22:55:31 -0500

    ChallengesList: Created the carousel to show all challenges LeftNav: Creater the Left Navigation bar to CMS index: Add some routes Challege: modify sponsores by sponsors

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/Home';
+import CMS from './pages/Admin/CMS';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 /*import App from './App';
 import './css/App.css';
@@ -12,6 +13,10 @@ ReactDOM.render((
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home}/>
+            <Route path ='/Admin' componet={CMS}/>"
+            <Route path ='/Admin/CMS/Challenge' componet={CMS}/>"
+            <Route path ='/Admin/CMS/Events' componet={CMS}/>"
+            <Route path ='/Admin/CMS/Users' componet={CMS}/>"
         </Switch>
     </BrowserRouter>)
 ,document.getElementById('root')
