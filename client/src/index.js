fba038da33920f6ec3e37bae757a9d21977486ed
Roberto Saldana <rob.salda@gmail.com>
2017-07-23 22:47:58 -0500

    Add Kit page to router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/Home';
+import Kit from './pages/Kit';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 /*import App from './App';
 import './css/App.css';
@@ -14,6 +15,7 @@ ReactDOM.render((
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home}/>
+            <Route exact path="/kit" component={Kit}/>
         </Switch>
     </BrowserRouter>)
 ,document.getElementById('root')
