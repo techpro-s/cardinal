2c24841f6dc8956e12f975211686bc49d7248967
Roberto Saldana <roberto.saldana@wizeline.com>
2017-09-10 00:11:10 -0500

    Add security to project. Still failing

--- app/server/grails-app/init/app/BootStrap.groovy
+++ app/server/grails-app/init/app/BootStrap.groovy
@@ -3,6 +3,9 @@ package app
 class BootStrap {
 
     def init = { servletContext ->
+        def role1 = new Authority(authority:"ROLE_USER").save flush:true
+        def user1 = new User(username:"user1",password:"pwd1").save flush:true
+        UserAuthority.create(user1,role1)
     }
     def destroy = {
     }
