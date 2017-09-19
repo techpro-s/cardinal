ee33c71db31a3bab897af5992de988235801728b
Roberto Saldana <roberto.saldana@wizeline.com>
2017-09-19 00:26:08 -0500

    Fix issues with memory and yarn

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,7 @@ RUN rm -fr /app
 # Copy App files
 COPY app /app
 
+ENV GRADLE_OPTS="-Xmx64m -Dorg.gradle.jvmargs='-Xmx256m -XX:MaxPermSize=64m'"
+
 # Add start command
 CMD ["./gradlew", "-Dgrails.env=prod", "bootRun", "-parallel"]
--- app/client/src/config.js
+++ app/client/src/config.js
@@ -1,5 +1,5 @@
 import pjson from './../package.json';
 
-export const SERVER_URL = 'http://18.220.86.128:8080/';
+export const SERVER_URL = 'http://13.59.69.116:8080/';
 export const CLIENT_VERSION = pjson.version;
 export const REACT_VERSION = pjson.dependencies.react;
